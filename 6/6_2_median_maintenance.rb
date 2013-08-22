225595f85f04bc18d7820347a8daf70e2d34a21e
Slavik <slavik@slavik-Aspire-5630.(none)>
2013-08-22 01:16:27 -0400

    Assignment 6. Custom Heap implementation. Work in progress

--- 6/6_2_median_maintenance.rb
+++ 6/6_2_median_maintenance.rb
@@ -4,6 +4,97 @@ require 'debugger'
 left_half_heap  = Containers::MaxHeap.new
 right_half_heap = Containers::MinHeap.new
 
+# Heap is stored as array of elements
+# 1st element - root, 2nd, 3rd elements - 2nd level
+# 4th, 5th, 6th, 7th elements - 3rd level, etc
+class Heap
+  MIN = 1; MAX = 2
+  def initialize(type=Heap::MIN)
+    @container = Array.new
+    @type = type 
+  end
+
+  def push element
+    @container << element
+    index = @container.rindex
+    bubble_up(index)
+  end
+
+  def pop
+    element = next()
+    
+  end
+
+  def next
+    @container[0]
+  end
+
+  private
+  def bubble_up index
+    element = @container[index]
+    parent_index = get_parent_index(index)
+    parent_element = @container[parent_index]
+    if parent_element.present?
+      if (@type == Heap::MIN and element < parent_element) or
+         (@type == Heap::MAX and element > parent_element)
+        swap(index, parent_index)
+        bubble_up(parent_index)
+      end
+    end
+  end
+
+  def bubble_down index
+    element = @container[index]
+    child_index = get_smallest_child_index(index)
+    child_element = @container[child_index]
+    if child_element.present?
+      if (@type == Heap::MIN and element > child_element) or
+         (@type == Heap::MAX and element < child_element)
+        swap(index, child_index)
+        bubble_down(child_index)
+      end
+    end
+  end
+
+  def swap x_index, y_index
+    return if x_index == y_index
+    element = @container[x_index]
+    @container[x_index] = @container[y_index]
+    @container[y_index] = element
+  end
+
+  def get_parent_index index
+    ((index+1)/2).floor - 1
+  end
+
+  def get_children_indexes index
+    left_child_index  = (index+1)*2-1
+    right_child_index = left_child_index+1
+    [left_child_index, right_child_index]
+  end
+
+  def get_smallest_child_index index
+    children_indexes = get_children_indexes index
+    left_child = @container[children_indexes[0]]
+    right_child = @container[children_indexes[1]]
+    smallest_child_index = nil
+    if left_child.nil? and right_child.nil?
+      smallest_child_index = nil
+    elsif left_child.nil?
+      smallest_child_index = children_indexes[1]
+    elsif right_child.nil?
+      smallest_child_index = children_indexes[0]
+    elsif left_child < right_child
+      smallest_child_index = children_indexes[0]
+    elsif right_child < left_child
+      smallest_child_index = children_indexes[1]
+    elsif right_child == left_child
+      smallest_child_index = children_indexes[0]
+    end
+    smallest_child_index
+  end
+end
+
 medianas = Array.new 10001
 
 i = 0
